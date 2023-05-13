a7d377b7d2ca2c47a624460b312e7f3e7d3ac8b8
Andre Eidi Maeda <a256321@dac.unicamp.br>
2023-05-13 16:30:35 -0300

    Readme, comentarios no código e mudança no num. maximo de estudantes

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# MC504
+# MC504 - Projeto Multithread 
+
+## Projeto dedicado a disciplina MC504 (Sistemas Operacionais) no primeiro semestre de 2023, ministrado pela professora [![Islene Calciolari Garcia](https://www.ic.unicamp.br/~islene/)](https://www.ic.unicamp.br/~islene/). 
+
+
+Esse projeto consiste na aplicação aplicação prática de conceitos explorados em aula sobre threads. Nesse sentido, com esse conteúdo em mente idealizamos um problema a ser resolvido usando threads, semáforos, entre outras definições vistas em aula. 
+
+## O Problema 
+
+O problema pensado foi adaptado de um dos exemplos apresentados no livro [![The Little Book of Semaphores](https://greenteapress.com/wp/semaphores/)](https://greenteapress.com/wp/semaphores/) seção 7.3, "The Room party Problem".
+
+Nessa perspectiva, o problema idealizado por nosso grupo consiste em uma adaptação, como dito anteriormente, do problema mencionado. Sendo assim, temos a seguinte situação: Um(a) professor(a) chega em sua sala para dar aula, mas é interrompido continuamente por alunos de outras salas que se alocam na frente da sala de sua turma. Ao ultrapassar um certo número de alunos gerando muito barulho, o(a) professor(a), incomodado(a), se desloca até a porta para expulsar os alunos de lá
+
+Dessa forma, teremos algumas restrições:
+- Teremos no máximo 20 estudantes que podem se alocar na porta da sala (note que este número é variável e pode ser mudado no código)
+- O professor pode expulsar as pessoas da porta de sua sala somente quando o número de estudantes em sua porta for maior ou igual a 10
+- Enquanto o professor estiver expulsando as pessoas de sua porta nenhum estudante pode se alocar para a frente da mesma
+- O professor não sai da porta até que todos os estudantes tenham saido da porta 
+- Há apenas um professor, isto é, pessoa que pode expulsar os alunos da porta
--- semaphore.c
+++ semaphore.c
@@ -5,30 +5,50 @@
 #include <string.h>
 #include <unistd.h>
 
-#define NUM_STUDENTS 60
+// Define o número máximo de estudantes que podem se acumular na porta da sala
+#define NUM_STUDENTS 20
+// Define a quantidade de estudantes que devem estar na porta para que o professor sinta-se incomodado para expulsá-los
+#define MAX_STUDENTS_DOOR 10
 #define MAX_DELAY 3
 
+// O mutex evita que as threads utilizem variaveis ao mesmo tempo, visto que cada função (professor e estudante) disputam as variaveis abaixo
 sem_t mutex;
+// Variavel atômica que indica quantos alunos estão alocados na porta da sala de aula
 int num_students = 0;
+// Indica a situação do professor no momento
 char acao_prof[20] = "dando aula";
 
-void *f_estudante(void *v) {
+void *f_estudante(void *v)
+{
+  // Recebe como argumento o id do aluno
   int id = *(int *)v;
+  // Variavel dentro sinaliza se o aluno de id x já está alocado na porta do professor
   int dentro = 0;
-  while (1) {
+  while (1)
+  {
     sleep(rand() % MAX_DELAY + 1);
     sem_wait(&mutex);
-    if (strcmp(acao_prof, "dando aula") == 0 && dentro != 1) {
+    if (strcmp(acao_prof, "dando aula") == 0 && dentro != 1)
+    {
+      // Aluno se coloca na porta do professor enquanto o professor dá a aula
       num_students++;
       dentro = 1;
       printf("estudante %d está na porta\n", id);
       sleep(1);
-    } else if (strcmp(acao_prof, "expulsando") == 0 && dentro == 1) {
+    }
+    else if (strcmp(acao_prof, "expulsando") == 0 && dentro == 1)
+    {
+      // O aluno sai da sala enquanto o professor está a expulsá-los
+      // Vale mencionar que nenhum aluno entra na sala enquanto o professor realiza esta ação
       num_students--;
       dentro = 0;
       printf("estudante %d saiu da porta\n", id);
       sleep(1);
-    } else if (dentro == 1) {
+    }
+    else if (dentro == 1)
+    {
+      // Se o aluno já está na porta apenas é sinalizado que ele está conversando/atrapalhando a aula
+      // Isso acontece pela existencia do while, sendo assim mesmo já estando na porta continua disputando o mutex
       printf("estudante %d está conversando na porta\n", id);
       sleep(3);
     }
@@ -36,21 +56,34 @@ void *f_estudante(void *v) {
   }
 }
 
-void *f_prof(void *v) {
-  while (1) {
+void *f_prof(void *v)
+{
+  while (1)
+  {
     sem_wait(&mutex);
-    if (strcmp(acao_prof, "dando aula") == 0) {
-      if (num_students >= 50) {
+    if (strcmp(acao_prof, "dando aula") == 0)
+    {
+      if (num_students >= MAX_STUDENTS_DOOR)
+      {
+        // Caso o professor tenha "pegado" o mutex e o número de estudantes for maior que a tolerância e começa a expulsá-los
         strcpy(acao_prof, "expulsando");
         printf("prof está expulsando os alunos barulhentos\n");
-      } else if (num_students == 0) {
+      }
+      else if (num_students == 0)
+      {
+        // Como o prof começa na situação "Dando aula" este será a primeira situação do problema, no caso o prof entra para começar a aula
         strcpy(acao_prof, "entrando...");
         printf("prof entrou na sala\n");
       }
-    } else if (strcmp(acao_prof, "expulsando") == 0 && num_students == 0) {
+    }
+    else if (strcmp(acao_prof, "expulsando") == 0 && num_students == 0)
+    {
+      // Quanto tiver terminado de expulsá-los retoma a aula normalmente
       strcpy(acao_prof, "dando aula");
       printf("prof voltou para a sala\n");
-    } else if (strcmp(acao_prof, "entrando...") == 0) {
+    }
+    else if (strcmp(acao_prof, "entrando...") == 0)
+    {
       sleep(5);
       printf("prof está dando aula\n");
       strcpy(acao_prof, "dando aula");
@@ -60,20 +93,24 @@ void *f_prof(void *v) {
   }
 }
 
-int main() {
+int main()
+{
   pthread_t thr_students[NUM_STUDENTS], thr_prof;
   int ids[NUM_STUDENTS];
 
+  // Mutex começa com valor 1 para poder ser consumido
   sem_init(&mutex, 0, 1);
 
   pthread_create(&thr_prof, NULL, f_prof, NULL);
-  for (int i = 0; i < NUM_STUDENTS; i++) {
+  for (int i = 0; i < NUM_STUDENTS; i++)
+  {
     ids[i] = i;
     pthread_create(&thr_students[i], NULL, f_estudante, &ids[i]);
   }
 
   pthread_join(thr_prof, NULL);
-  for (int i = 0; i < NUM_STUDENTS; i++) {
+  for (int i = 0; i < NUM_STUDENTS; i++)
+  {
     pthread_join(thr_students[i], NULL);
   }
 
